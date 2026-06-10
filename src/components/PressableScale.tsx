b461fb316cf62561f6c51ed297c33a9e4af3c3e1
Pedro Souza <pedro.souza4237@gmail.com>
2026-06-10 14:56:34 -0300

    feat(ui): PressableScale, GradientButton e ProgressBar

---
+++ src/components/PressableScale.tsx
@@ -0,0 +1,32 @@
+import { Pressable, type PressableProps } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
+
+type Props = PressableProps & { children: React.ReactNode }
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
+export function PressableScale({ children, onPressIn, onPressOut, ...props }: Props) {
+  const scale = useSharedValue(1)
+  const animStyle = useAnimatedStyle(() => ({ transform: [{ scale: scale.value }] }))
+
+  return (
+    <AnimatedPressable
+      style={animStyle}
+      onPressIn={e => {
+        scale.value = withSpring(0.96, { damping: 15, stiffness: 300 })
+        onPressIn?.(e)
+      }}
+      onPressOut={e => {
+        scale.value = withSpring(1, { damping: 15, stiffness: 300 })
+        onPressOut?.(e)
+      }}
+      {...props}
+    >
+      {children}
+    </AnimatedPressable>
+  )
+}
